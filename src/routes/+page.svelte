f0f6ead5adade4311ddd53fb298fcbc7c3421d49
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-10-17 01:06:41 +0300

    Apply file project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class=" text-center text-3xl bg-red-500 font-bold underline">
+  Senior Project!
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
